48dd98fbb744cf2f1a95e61b582dba54952835f1
root <root@west69101.(none)>
2013-09-07 11:38:31 +0800

    by jiangzhibin

--- user.php
+++ user.php
@@ -1,5 +1,4 @@
 <?php
-/*
 $_httprootdir = '/var/www/html';
 require_once ($_httprootdir.'/app/common/User.php');
 $_user = Common_User::current();
@@ -11,5 +10,5 @@ $_loginurl="http://www.ourfreesky.org/bbs/login.php";
 #else{
 #	echo 'current user :' . $_user['name'];
 #}
-require("constants.php");*/
+require("constants.php");
 ?>
