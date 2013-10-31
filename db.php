b7be94b3a3bbb96a94e583bd318bc58676ca25ff
jianghust <jianghust1988@126.com>
2013-10-31 22:19:51 +0800

    by jiangzhibin

--- db.php
+++ db.php
@@ -1,8 +1,13 @@
 <?php
-	require_once  'lib/meekrodb.2.2.class.php';
+	//require_once  '/var/www/html/teacher/lib/meekrodb.2.2.class.php';
+	//require_once '../app/common/lib/meekrodb.2.1.class.php';
 //lib/meekrodb.2.2.class.php';
+//	DB::disconnect();
 	DB::$user = 'yinhang';
 	DB::$password = 'ofs@2006';
 	DB::$dbName = 'teacher';
+//	DB::get('teacher');
+	DB::useDB('teacher');
+//	DB::get();
 	$APPLYTABLE = DB::$dbName.".apply";
 ?>
--- user.php
+++ user.php
@@ -1,14 +1,12 @@
 <?php
-$_httprootdir = dirname(__FILE__) . '../../';
+$_httprootdir = dirname(__FILE__) . '/..';
 require_once ($_httprootdir .'/app/common/User.php');
-$_user = Common_User::current();
+$_user  = Common_User::current();
 $_loginurl="http://www.ourfreesky.org/bbs/login.php";
-
 #if($_user['is_login'] == false){
 #  echo '<a href="http://www.ourfreesky.org/bbs/login.php">login</a>';
 #}
 #else{
 #	echo 'current user :' . $_user['name'];
 #}
-require("constants.php");
 ?>
