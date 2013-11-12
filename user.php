9fa163282fca60ef12746e95c98d1357453f2e4d
jianghust <jianghust1988@126.com>
2013-11-12 21:22:14 +0800

    by jiangzhibin

--- user.php
+++ user.php
@@ -3,10 +3,11 @@ $_httprootdir = dirname(__FILE__) . '/..';
 require_once ($_httprootdir .'/app/common/User.php');
 $_user  = Common_User::current();
 $_loginurl="http://www.ourfreesky.org/bbs/login.php";
-#if($_user['is_login'] == false){
-#  echo '<a href="http://www.ourfreesky.org/bbs/login.php">login</a>';
-#}
-#else{
-#	echo 'current user :' . $_user['name'];
-#}
+if($_user['is_login'] == false){
+  echo '<a href="http://www.ourfreesky.org/bbs/login.php">login</a>';
+}
+else{
+	echo 'current user :' . $_user['name'];
+	var_dump($_user);
+}
 ?>
